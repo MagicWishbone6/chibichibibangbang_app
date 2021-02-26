bf3cf732cc8b6cb2b77c4ca69499b373c090bf91
Caitlin <caitlinlamprecht@outlook.com>
2021-02-25 21:08:51 -0600

    added react-image-crop

--- chibichibi_frontend/src/components/Home.jsx
+++ chibichibi_frontend/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CropTool from './CropTool'
 
 function Home() {
     return (
         <div>
             <h4>Home Component</h4>
+            <CropTool />
         </div>
     );
 }
