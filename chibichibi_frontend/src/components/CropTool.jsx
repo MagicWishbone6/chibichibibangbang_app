40fea04a9309d7fc92fa0f7a895ca2936adebe7c
Caitlin <caitlinlamprecht@outlook.com>
2021-03-01 17:21:58 -0600

    restored front to clone to front repo

---
+++ chibichibi_frontend/src/components/CropTool.jsx
@@ -0,0 +1,125 @@
+// from https://www.npmjs.com/package/react-image-crop#installation
+import React, { PureComponent } from 'react';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+import CustomInput from './muik-components/CustomInput/CustomInput'
+import GridItem from './muik-components/Grid/GridItem'
+import GridContainer from './muik-components/Grid/GridContainer'
+
+class CropTool extends PureComponent {
+  state = {
+    src: null,
+    crop: {
+      unit: '%',
+      width: 30,
+      aspect: 16 / 16,
+    },
+  };
+
+  onSelectFile = e => {
+    if (e.target.files && e.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () =>
+        this.setState({ src: reader.result })
+      );
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  // If you setState the crop in here you should return false.
+  onImageLoaded = image => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = crop => {
+    this.makeClientCrop(crop);
+  };
+
+  onCropChange = (crop, percentCrop) => {
+    // You could also use percentCrop:
+    // this.setState({ crop: percentCrop });
+    this.setState({ crop });
+  };
+
+  async makeClientCrop(crop) {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageUrl = await this.getCroppedImg(
+        this.imageRef,
+        crop,
+        'newFile.jpeg'
+      );
+      this.setState({ croppedImageUrl });
+    }
+  }
+
+  getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error('Canvas is empty');
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, 'image/jpeg');
+    });
+  }
+
+  render() {
+    const { crop, croppedImageUrl, src } = this.state;
+
+    return (
+      <GridContainer>
+        <GridItem>
+            {/* <CustomInput
+                formControlProps={{
+                    fullWidth: true
+                }}> */}
+                <input type="file" accept="image/*" onChange={this.onSelectFile} />
+            {/* </CustomInput> */}
+        </GridItem>
+        <GridItem>
+        {src && (
+          <ReactCrop
+            src={src}
+            crop={crop}
+            ruleOfThirds
+            onImageLoaded={this.onImageLoaded}
+            onComplete={this.onCropComplete}
+            onChange={this.onCropChange}
+          />
+        )}
+        </GridItem>
+        <GridItem>
+        {croppedImageUrl && (
+          <img alt="Crop" style={{ maxWidth: '100%' }} src={croppedImageUrl} />
+        )}
+        </GridItem>
+      </GridContainer>
+    );
+  }
+}
+
+export default CropTool
---
+++ chibichibi_frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CropTool from './CropTool'
+
+function Home() {
+    return (
+        <div>
+            <h4>Home Component</h4>
+            <CropTool />
+        </div>
+    );
+}
+
+export default Home;
