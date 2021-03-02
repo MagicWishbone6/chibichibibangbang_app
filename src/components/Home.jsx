902107560085b1edb51bcbe5bf40331ecb8451c8
Caitlin <caitlinlamprecht@outlook.com>
2021-03-02 09:09:54 -0600

    removed crop tool from Home comp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import CropTool from './CropTool'
 
 function Home() {
     return (
         <div>
             <h4>Home Component</h4>
-            <CropTool />
         </div>
     );
 }
