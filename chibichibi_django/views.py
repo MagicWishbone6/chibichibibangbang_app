585acd159e17fb0abc1449ef41e283d20e32d7d1
Caitlin <caitlinlamprecht@outlook.com>
2021-03-02 09:03:45 -0600

    removed backend dirs

--- chibichibi_django/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import generics
-from django.views.generic import TemplateView
-from django.views.decorators.cache import never_cache
-
-# Serve Single Page Application
-index = never_cache(TemplateView.as_view(template_name='index.html'))
--- chibichibibangbang/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Character
-
-class CharacterForm(forms.ModelForm):
-    class Meta:
-        model = Character
-        fields = ('name', 'franchise', 'artist', 'source_url', 'image_url')
